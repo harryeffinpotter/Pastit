28f8e12d452ab9b89ca28f58267544d8c9f5f7f9
yermom <me@yourmom.net>
2024-07-12 02:20:37 -0500

    boogalee - the boogaloo three

--- setup.sh
+++ setup.sh
@@ -1,23 +1,40 @@
 #!/bin/bash
 
-# Path to the main script
-main_script="pastit"
-
+mainscript="pastit"
 
 cat ./pastit | grep "ENTER_YOUR_ZIPLINE"
-if [[ "$?" == 1 ]] || [[ -z "/usr/local/bin/pastit" ]]; then
-echo "Already configured, exiting!"
-exit 0
+if [[ "$?" == 1 ]] || [[ -f '/usr/local/bin/pastit' ]]; then
+    echo "Already configured, exiting!"
+    exit 0
 fi
 clear
+
+# Function to format the URL correctly
+format_url() {
+    local input_url=$1
+    # Remove existing /api/upload if it appears
+    formatted_url=$(echo "$input_url" | sed 's#/api/upload##g')
+
+    # Add https:// if not present
+    if [[ ! $formatted_url =~ ^http ]]; then
+        formatted_url="https://$formatted_url"
+    fi
+
+    # Ensure there's only one forward slash at the end before appending /api/upload
+    formatted_url=$(echo "$formatted_url" | sed 's#/*$##')
+    formatted_url="$formatted_url/api/upload"
+
+    echo "$formatted_url"
+}
+
 # Check if zipline.sxcu file exists in the same directory
 zipline_file="zipline.sxcu"
 if [ ! -f "$zipline_file" ]; then
     echo "ERROR: No reference file found."
     echo -e "\nYou can either -\n1. Paste your details manually into this script in the following prompt.\n2. Put your generated zipline.sxcu file in the same directory as this script and run it again.\n3. Manually edit the main script yourself and supply the necessary infomation, then move it to /usr/local/bin and make it executable.\n\n"
-    sleep 10
-    echo -e "If you chose 2 or 3, press CTRL+C to exit. If you want to paste in the data, CTRL-Left click this link:" 
-    printf '\e]8;;https://share.harryeffingpotter.com/u/QHqMKf.mp4\e\\Follow along with this 7 second video guide\e]8;;\e\\\n' 
+    sleep 3
+    echo -e "If you chose 2 or 3, press CTRL+C to exit. If you want to paste in the data, CTRL-Left click this link:"
+    printf '\e]8;;https://share.harryeffingpotter.com/u/QHqMKf.mp4\e\\Follow along with this 7 second video guide\e]8;;\e\\\n'
     echo -e "- then paste in the result:"
     read authorizationtoken
     if [ -z "$authorizationtoken" ]; then
@@ -33,39 +50,33 @@ if [ ! -f "$zipline_file" ]; then
         sleep 10
         exit 1
     fi
+    URL=$(format_url "$URL")
 else
     # Read the values from the zipline.sxcu file
     URL=$(jq -r '.RequestURL' "$zipline_file")
     authorizationtoken=$(jq -r '.Headers.Authorization' "$zipline_file")
 fi
 clear
-echo -e "Awesome! Looks like we have all we need!\n"
-sleep 2
-echo -e "\nHere is the gathered information:\n\nAPI url:\n${URL}\n\nAuthorization token:\n${authorizationtoken}\n\n...please verify that it is correct.\n\nAfter this point if this info is wrong - you must correct your pastit file at /usr/local/bin/pastit.\nYou can press CTRL+C now and try again to avoid this right now.\n\nScript will now pause while you verify this information, press ENTER when done to write your changes."
+echo -e "\nHere is what you entered:\n\nAPI url:\n${URL}\n\nAuthorization token:\n${authorizationtoken}\n\nIF THIS IS INCORRECT - PRESS CTRL+C and start over, otherwise PRESS ENTER."
 read NOTHING
 echo "Now editing pastit script with your information!"
 # Replace placeholders in the main script
-sed -i "s|ENTER_YOUR_ZIPLINE_URL_HERE/api/upload|$request_url|g" "$main_script"
-sed -i "s|ENTER_YOUR_ZIPLINE_AUTHORIZATION_TOKEN_HERE|$authorization_token|g" "$main_script"
+sed -i "s|ENTER_YOUR_ZIPLINE_URL_HERE/api/upload|$URL|g" "$mainscript"
+sed -i "s|ENTER_YOUR_ZIPLINE_AUTHORIZATION_TOKEN_HERE|$authorizationtoken|g" "$mainscript"
 
 echo "Main script updated with URL and Authorization token."
 
+echo -e "\n\nFinalizing!\n\nRIP SPRUNGE, YOU WERE IMMEDIATELY MISSED. WHY DID YOU LEAVE US LIKE THIS, NOT ONCE, BUT TWICE!\n\nEven though you most definitely already know about them if you're here, it still is worth saying:\n\nBIG THANKS TO ZIPLINE DEVS FOR THIS INCREDIBLE, FREE, AMAZING REPO, SHOW THEM SOME LOVE PLEASE\n\n https://github.com/diced/zipline\n\nBUY THOSE BOYS A COFFEE FOR FUCKS SAKE!"
+sleep 8
+echo -e "\n\nHOW TO USE PASTIT:\n\nSTDOUT:\necho \"pastitties\" | pastit\n\nPASTE FILE:\npastit ~/.zshrc\n\nFINISHED! COPYING EXECUTABLE..."
 
-echo -e "\n\nIf you wish to change the default extension replacement for extensionless files you can find that on LINE 15, and a setting to decide whether or not to count hidden files (files that start with a \'.\' like .zshrc, .config files, etc) as extensionless on LINE 18\n\nThe default settings are .sh and true, this is so files like .zshrc have proper bash syntax highlighting, but ultimately - it's up to you."
-
-
-echo -e "\n\nFinalizing - copying script to /usr/local/bin!\n\nRIP SPRUNGE, YOU WERE IMMEDIATELY MISSED. WHY DID YOU LEAVE US LIKE THIS, NOT ONCE, BUT TWICE, MY HEART CAN NO LONGER TAKE IT. I HAVE MOVED ON.\n\nand even though you most definitely already know about them if you're here, it still is worth saying:\n\nBIG THANKS TO ZIPLINE DEVS FOR THIS INCREDIBLE, FREE, AMAZING REPO, SHOW THEM SOME LOVE PLEASE https://github.com/diced/zipline, BUY THOSE BOYS A COFFEE FOR FUCKS SAKE!"
-sleep 10
-echo -e "\n\nHOW TO USE PASTIT:\n\nSTDOUT:\necho "pastitties" | pastit\n\nPASTE FILE:\npastit ~/.zshrc\n\nFINISHED! COPYING EXECUTABLE..."
- 
 chmod +x ./pastit
-cp pastit /usr/local/bin/pastit
+sudo cp ./pastit /usr/local/bin/pastit
 
 check_path() {
     echo $PATH | grep -q "/usr/local/bin"
     return $?
 }
-
 
 # Function to append /usr/local/bin to .bashrc and .zshrc
 append_to_files() {
@@ -73,7 +84,6 @@ append_to_files() {
     echo 'export PATH="/usr/local/bin:$PATH"' >> ~/.zshrc
     echo "/usr/local/bin has been added to your PATH in .bashrc and .zshrc"
 }
-
 
 check_path
 if [ $? -ne 0 ]; then
